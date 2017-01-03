75e3edb041b0734496d09021ffffc7de40a86c8e
kazuhsat <kazuhsat@MBP13UAL-NC0135.local>
2017-01-03 11:40:20 +0900

    mod

--- src/Page.scala
+++ src/Page.scala
@@ -2,24 +2,14 @@ package com.mangaz.project.publisher
 
 import java.io.File
 
-class Page(pathname: String) {
-  val file = new File(pathname)
-
+class Page(pathname: String) extends File(pathname: String) {
   def getNameExtractExtension(): String = {
-    val filename = file.getName
+    val filename = getName
 
     val point: Int = filename.lastIndexOf(".");
     if (point == -1)
         return null
 
     filename.substring(0, point);
-  }
-
-  def getAbsolutePath(): String = {
-    file.getAbsolutePath
-  }
-
-  def getName(): String = {
-    file.getName
   }
 }
